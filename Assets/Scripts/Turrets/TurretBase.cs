1a4231db60da26b0c29f7912e4e66f684ffca352
Walking-Glitch <84610569+Walking-Glitch@users.noreply.github.com>
2025-01-12 02:45:28 -0500

    Turret update 1
    + Turret 3d models added.
    + Turrets keep track of enemies in area of fire.
    + Turrets rotate and adjust pitch towards the current target.

---
+++ Assets/Scripts/Turrets/TurretBase.cs
@@ -0,0 +1,252 @@
+using Assets.Scripts.Game_Manager;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TurretBase : MonoBehaviour
+{
+    // weapon stats
+    public int WeaponDamage;
+    public int WeaponFireRate;
+    public float WeaponRange;
+  
+
+    //  layers
+    public LayerMask ZombieLayer;
+    public LayerMask ShootMask;
+
+    // game manager reference
+    protected GameManager gameManager;
+
+    // list of enemies in firing area
+    [SerializeField] protected List<ZombieStateManager> enemies = new List<ZombieStateManager>();
+
+    // turret transforms  
+    public Transform PitchTransform;
+    public Transform PanTransform;
+    public Transform GunEndTransform;
+    public float RotationSpeed;
+
+    public ZombieStateManager currentEnemy;
+
+    // hit decals
+    public GameObject hitGroundDecal;
+    [HideInInspector] public Vector3 ShotForceDir;
+
+    // laser variables
+    [SerializeField] private bool laserReady;
+    public LineRenderer laserLine;
+    public Transform laserOrigin;
+
+    public float range;
+
+
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindEnemiesInRange();
+
+        AimAtTarget();
+
+        RotateToDefaultPosition();
+
+        if (currentEnemy != null)
+        {
+            laserLine.enabled = true;
+            laserLine.SetPosition(0, laserOrigin.position);
+            laserLine.SetPosition(1, currentEnemy.transform.position);
+        }
+    }
+
+    protected virtual void RotateToDefaultPosition()
+    {
+        if (currentEnemy == null)
+        {
+            // Define the default direction or rotation
+            Vector3 defaultDirection = transform.forward;  
+            Vector3 horizontalDefaultDirection = new Vector3(defaultDirection.x, 0, defaultDirection.z).normalized;
+
+            if (horizontalDefaultDirection != Vector3.zero)
+            {
+                Quaternion horizontalRotation = Quaternion.LookRotation(horizontalDefaultDirection);
+                PanTransform.rotation = Quaternion.Slerp(PanTransform.rotation, horizontalRotation, Time.deltaTime * RotationSpeed);
+            }
+
+            
+            if (PitchTransform != null)
+            {
+                Quaternion neutralPitchRotation = Quaternion.Euler(0, PitchTransform.rotation.eulerAngles.y, 0);
+                PitchTransform.rotation = Quaternion.Slerp(PitchTransform.rotation, neutralPitchRotation, Time.deltaTime * RotationSpeed);
+            }
+        }
+    }
+    protected virtual void AimAtTarget()
+    {
+        if(enemies.Count > 0)
+        {
+            if (currentEnemy == null || !enemies.Contains(currentEnemy))
+            {
+                currentEnemy = enemies[0];
+            }
+
+            Vector3 direction = currentEnemy.transform.position - transform.position;
+
+            Vector3 horizontalDirection = new Vector3(direction.x, 0, direction.z);
+
+            Vector3 verticalDirection = new Vector3(0, direction.y, 0);
+
+
+            if (horizontalDirection != Vector3.zero)
+            {
+                Quaternion horizontalRotation = Quaternion.LookRotation(horizontalDirection);
+                PanTransform.rotation = Quaternion.Slerp(PanTransform.rotation, horizontalRotation, Time.deltaTime * RotationSpeed);
+            }
+
+            if (PitchTransform != null)
+            {
+                Vector3 targetPosition = currentEnemy.transform.position;
+                Vector3 barrelDirection = targetPosition - PitchTransform.position;
+
+                Quaternion verticalRotation = Quaternion.LookRotation(barrelDirection);
+                PitchTransform.rotation = Quaternion.Slerp(PitchTransform.rotation, verticalRotation, Time.deltaTime * RotationSpeed);
+
+                // Lock the barrel's horizontal rotation to match the turret base
+                PitchTransform.rotation = Quaternion.Euler(PitchTransform.rotation.eulerAngles.x, PanTransform.rotation.eulerAngles.y, 0);
+            }
+
+
+            // Check alignment for firing
+            float horizontalAlignment = Vector3.Dot(PanTransform.forward, horizontalDirection);
+            float verticalAlignment = Vector3.Dot(PitchTransform.up, verticalDirection);
+
+            if (horizontalAlignment >= 0.99f && verticalAlignment >= 0.99f)
+            {
+                Fire();
+                Debug.Log("FIRING");
+            }
+        }
+
+        else
+        {
+            currentEnemy = null;          
+        }
+
+    }
+       
+
+    protected virtual void FindEnemiesInRange()
+    {
+        enemies.Clear();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, WeaponRange, ZombieLayer);
+
+        foreach (Collider col in colliders)
+        {
+            ZombieStateManager zombie = col.gameObject.GetComponentInParent<ZombieStateManager>();
+
+            if (col.gameObject.GetComponentInParent<ZombieStateManager>().health > 0 && !enemies.Contains(zombie))
+                enemies.Add(col.GetComponentInParent<ZombieStateManager>());
+        }
+    }
+
+    //protected virtual bool CanTurretFire()
+    //{
+    //    if(currentEnemy == null) return false;
+    //}
+    void Fire()
+    {
+        
+         
+        Vector3 direction = currentEnemy.transform.position - GunEndTransform.position;
+
+        if (Physics.Raycast(GunEndTransform.position, direction.normalized, out RaycastHit hit, Mathf.Infinity,
+                ShootMask))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                Quaternion decalRotation = Quaternion.LookRotation(hit.normal);
+
+                Instantiate(hitGroundDecal, hit.point, decalRotation);
+            }
+
+            else if (hit.collider.CompareTag("Zombie"))
+            {
+                Quaternion decalRotation = Quaternion.LookRotation(hit.normal);
+                Instantiate(hitGroundDecal, hit.point, decalRotation);
+
+                ZombieStateManager zombieStateManager;
+
+                Limbs limb = hit.collider.GetComponent<Limbs>();
+
+                ShotForceDir = hit.normal * -1;
+
+                if (limb != null)
+                {
+                    float baseDamage = 40;
+                    float finalDamage = baseDamage * limb.damageMultiplier;
+
+                    if (limb.limbName == "head")
+                    {
+                        zombieStateManager = hit.collider.GetComponentInParent<ZombieStateManager>();
+                        zombieStateManager.TakeDamage((int)finalDamage, limb.limbName, false, 0);
+                    }
+
+                    else if (limb.limbName == "torso" || limb.limbName == "belly")
+                    {
+                        zombieStateManager = hit.collider.GetComponentInParent<ZombieStateManager>();
+                        zombieStateManager.TakeDamage((int)finalDamage, limb.limbName, false, 0);
+                    }
+
+                    else
+                    {
+                        zombieStateManager = hit.collider.GetComponentInParent<ZombieStateManager>();
+                        zombieStateManager.TakeDamage((int)finalDamage, limb.limbName, false, 0);
+                    }
+
+
+                    float baseLimbDmg = 100f;
+                    float finalLimbDmg = baseLimbDmg * limb.limbDamageMultiplier;
+
+                    limb.LimbTakeDamage((int)finalLimbDmg);
+
+                    if (limb.limbReplacement != null && limb.limbHealth <= 0)
+                    {
+
+                        Rigidbody rb = limb.limbReplacement.GetComponent<Rigidbody>();
+                        Transform trans = limb.limbReplacement.transform;
+                        Vector3 backwardForce = direction * 10f;
+                        rb.AddForce(backwardForce, ForceMode.Impulse);
+                        Debug.Log("force added");
+                        Debug.Log("Force direction: " + direction);
+                        Debug.DrawRay(trans.position, direction, Color.red, 2f);
+                    }
+                    Debug.Log(limb.limbName);
+                }
+
+            }
+
+            else
+            {
+                Debug.Log(hit.distance);
+            }
+        }
+
+        //aimStateManager.AddRecoil();
+    }
+
+    
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw a wire sphere in the editor to visualize the detection radius
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, WeaponRange);
+    }
+
+}
